64c0b8870252a41a948da677aa99119646099a40
James Little <littleguy23@gmail.com>
2018-03-21 11:19:28 +0100

    Add better default files

---
+++ secret_template.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Provide a $passwords variable here to map passwords to possible namespaces.
+ *
+ * The key should be a password and the value should be either an array of
+ * allowed namespaces or the string '*', which will allow any namespace to
+ * be used.
+ */
+$passwords = [
+	'replaceme!' => '*',
+	'otherpassword' => ['mynamespace', 'othernamespace']
+];
